b69987fb96339d8f449efda2534671f436ea7342
Andre Febrianto <andrefebrianto88@gmail.com>
2024-02-07 12:39:34 +0700

    feat: implement middleware for validate jwt token

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/SawitProRecruitment/UserService/generated"
 	"github.com/SawitProRecruitment/UserService/handler"
+	"github.com/SawitProRecruitment/UserService/password"
 	"github.com/SawitProRecruitment/UserService/repository"
+	"github.com/SawitProRecruitment/UserService/token"
+	validatorwrapper "github.com/SawitProRecruitment/UserService/validator"
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4/middleware"
 
 	"github.com/labstack/echo/v4"
 )
@@ -13,19 +19,46 @@ import (
 func main() {
 	e := echo.New()
 
-	var server generated.ServerInterface = newServer()
+	tokenHandler := initTokenSigningKey()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+	e.Use(ValidateJWT(tokenHandler))
+
+	var server generated.ServerInterface = initServer(tokenHandler)
 
 	generated.RegisterHandlers(e, server)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func newServer() *handler.Server {
+func initTokenSigningKey() *token.Token {
+	prvKey, err := os.ReadFile("cert/id_rsa")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	pubKey, err := os.ReadFile("cert/id_rsa.pub")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return token.NewToken(prvKey, pubKey)
+}
+
+func initServer(tokenHandler *token.Token) *handler.Server {
 	dbDsn := os.Getenv("DATABASE_URL")
 	var repo repository.RepositoryInterface = repository.NewRepository(repository.NewRepositoryOptions{
 		Dsn: dbDsn,
 	})
+
+	passwordHandler := password.NewPassword(12)
+	inputValidator := validatorwrapper.NewValidator(validator.New())
+
 	opts := handler.NewServerOptions{
 		Repository: repo,
+		Token:      tokenHandler,
+		Password:   passwordHandler,
+		Validator:  inputValidator,
 	}
 	return handler.NewServer(opts)
 }
---
+++ cmd/middleware.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/SawitProRecruitment/UserService/constant"
+	"github.com/SawitProRecruitment/UserService/generated"
+	"github.com/SawitProRecruitment/UserService/token"
+
+	"github.com/labstack/echo/v4"
+)
+
+const (
+	authHeader = "Authorization"
+	prefix     = "Bearer "
+)
+
+func ValidateJWT(tokenHandler *token.Token) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			value := c.Request().Header.Values(authHeader)
+
+			if len(value) > 0 && strings.HasPrefix(value[0], prefix) {
+				accessToken := strings.TrimPrefix(value[0], prefix)
+				claim, err := tokenHandler.Validate(accessToken)
+				if err != nil {
+					return c.JSON(http.StatusUnauthorized, &generated.Error{Message: "The request lacks valid authentication credentials for the requested resource"})
+				}
+
+				c.Set(constant.AuthContextKey, claim)
+			}
+
+			return next(c)
+		}
+	}
+}
